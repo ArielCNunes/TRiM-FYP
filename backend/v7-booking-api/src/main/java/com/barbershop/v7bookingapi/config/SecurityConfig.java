2629569ee733880f847fded99ec6128f975914de
Ariel <arielc.nunes@outlook.com>
2025-10-07 21:13:43 +0100

    Change SecurityConfig file into appropriate module

--- backend/v7-booking-api/src/main/java/com/barbershop/v7bookingapi/config/SecurityConfig.java
+++ backend/v7-booking-api/src/main/java/com/barbershop/v7bookingapi/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.barbershop.v7bookingapi;
+package com.barbershop.v7bookingapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
